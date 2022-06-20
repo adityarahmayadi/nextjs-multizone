5226468583a30f03a94849af1758e09ddc0897ea
adityarahmayadi <aditya.rahmayadi@gmail.com>
2022-06-20 12:05:41 +0700

    feat: add tailwind navbar and card to homepage

--- home/pages/index.tsx
+++ home/pages/index.tsx
@@ -1,35 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Homepage from '../components/pages/homepage'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Homepage: Main Application</title>
-        <meta name="description" content="Main container for every zones" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <Homepage title="Main Application" />
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <Homepage title="Main Application" />
   )
 }
 
